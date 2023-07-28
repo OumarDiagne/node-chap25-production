e6c86cf03aaa7edb5d835bb239e17f5f165a8dd7
Diagne <oums4b@gmail.com>
2023-07-28 20:27:14 +0200

    changes for prod

--- environment/production.js
+++ environment/production.js
@@ -3,6 +3,6 @@ const path = require("path"); //module core de node.js
 module.exports = {
   dbUrl:
     "mongodb://gin:shinso@ac-wmzxzzo-shard-00-00.seixkun.mongodb.net:27017,ac-wmzxzzo-shard-00-01.seixkun.mongodb.net:27017,ac-wmzxzzo-shard-00-02.seixkun.mongodb.net:27017/twitter?ssl=true&replicaSet=atlas-vkyqdd-shard-0&authSource=admin&retryWrites=true&w=majority",
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
+  cert: "/etc/letsencrypt/live/www.shinso-projects.site/fullchain.pem",
+  key: "/etc/letsencrypt/live/www.shinso-projects.site/privkey.pem",
 };
